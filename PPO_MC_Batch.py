b435b7af4f84ca05324c6cdd564b92afc94f221f
alanyuwenche <alanyuwenche@gmail.com>
2023-12-23 21:18:30 +0800

    Add files via upload

--- PPO_MC_Batch.py
+++ PPO_MC_Batch.py
@@ -37,36 +37,36 @@ class RolloutBuffer:
 
 
 class ActorCritic(nn.Module):
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, hidden_dim):
         super(ActorCritic, self).__init__()
         self.actor = nn.Sequential(
-                        nn.Linear(state_dim, 64),
+                        nn.Linear(state_dim, hidden_dim),
                         nn.Tanh(),
-                        nn.Linear(64, 64),
+                        nn.Linear(hidden_dim, hidden_dim),
                         nn.Tanh(),
-                        nn.Linear(64, action_dim),
+                        nn.Linear(hidden_dim, action_dim),
                         nn.Softmax(dim=-1)
                     )
 
     
         # critic
         self.critic = nn.Sequential(
-                        nn.Linear(state_dim, 64),
+                        nn.Linear(state_dim, hidden_dim),
                         nn.Tanh(),
-                        nn.Linear(64, 64),
+                        nn.Linear(hidden_dim, hidden_dim),
                         nn.Tanh(),
-                        nn.Linear(64, 1)
+                        nn.Linear(hidden_dim, 1)
                     )
-        
+
     def act(self, state):
 
         action_probs = self.actor(state)
         dist = Categorical(action_probs)
         action = dist.sample()
         action_logprob = dist.log_prob(action)
-        
+
         return action.detach(), action_logprob.detach()
-    
+
 
     def evaluate(self, state, action):
 
@@ -75,35 +75,34 @@ class ActorCritic(nn.Module):
         action_logprobs = dist.log_prob(action)
         dist_entropy = dist.entropy()
         state_values = self.critic(state)
-        
+
         return action_logprobs, state_values, dist_entropy
 
 class PPO:
-    def __init__(self, state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip):
+    def __init__(self, state_dim, action_dim, hidden_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip):
         self.gamma = gamma
         self.eps_clip = eps_clip
         self.K_epochs = K_epochs
-        
-        self.buffer = RolloutBuffer()
 
-        self.policy = ActorCritic(state_dim, action_dim).to(device)
+        self.buffer = RolloutBuffer()
+        self.policy = ActorCritic(state_dim, action_dim, hidden_dim).to(device)
         self.optimizer = torch.optim.Adam([
                         {'params': self.policy.actor.parameters(), 'lr': lr_actor},
                         {'params': self.policy.critic.parameters(), 'lr': lr_critic}
                     ])
 
-        self.policy_old = ActorCritic(state_dim, action_dim).to(device)
+        self.policy_old = ActorCritic(state_dim, action_dim, hidden_dim).to(device)
         self.policy_old.load_state_dict(self.policy.state_dict())
-        
+
         self.MseLoss = nn.MSELoss()
 
- 
+
     def select_action(self, state):
 
         with torch.no_grad():
             state = torch.FloatTensor(state).to(device)
             action, action_logprob = self.policy_old.act(state)
-        
+
         self.buffer.states.append(state)
         self.buffer.actions.append(action)
         self.buffer.logprobs.append(action_logprob)
@@ -121,7 +120,7 @@ class PPO:
                 discounted_reward = 0
             discounted_reward = reward + (self.gamma * discounted_reward)
             rewards.insert(0, discounted_reward)
-            
+
         # Normalizing the rewards
         rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
         rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-7)
@@ -131,7 +130,7 @@ class PPO:
         old_actions = torch.squeeze(torch.stack(self.buffer.actions, dim=0)).detach().to(device)
         old_logprobs = torch.squeeze(torch.stack(self.buffer.logprobs, dim=0)).detach().to(device)
 
-        
+
         # Optimize policy for K epochs
         for _ in range(self.K_epochs):
 
@@ -140,38 +139,38 @@ class PPO:
 
             # match state_values tensor dimensions with rewards tensor
             state_values = torch.squeeze(state_values)
-            
+
             # Finding the ratio (pi_theta / pi_theta__old)
             ratios = torch.exp(logprobs - old_logprobs.detach())
 
             # Finding Surrogate Loss
-            advantages = rewards - state_values.detach()   
+            advantages = rewards - state_values.detach()
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
 
             # final loss of clipped objective PPO
             loss = -torch.min(surr1, surr2) + 0.5*self.MseLoss(state_values, rewards) - 0.01*dist_entropy
-            
+
             # take gradient step
             self.optimizer.zero_grad()
             loss.mean().backward()
             self.optimizer.step()
-            
+
         # Copy new weights into old policy
         self.policy_old.load_state_dict(self.policy.state_dict())
 
         # clear buffer
         self.buffer.clear()
-    
-    
+
+
     def save(self, checkpoint_path):
         torch.save(self.policy_old.state_dict(), checkpoint_path)
-   
+
 
     def load(self, checkpoint_path):
         self.policy_old.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
         self.policy.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
-        
+
 print("============================================================================================")
 
 def seed_torch(seed):
@@ -182,7 +181,6 @@ def seed_torch(seed):
 
 ####### initialize environment hyperparameters ######
 env_name = "MountainCar-v0"
-
 seed = 1
 np.random.seed(seed)
 random.seed(seed)
@@ -204,37 +202,33 @@ env = gym.make(env_name)
 # state space dimension
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
-
+#hidden_dim = 128 
+hidden_dim = 64 #20231222 較易成功
 ################# training procedure ################
 
 # initialize a PPO agent
-ppo_agent = PPO(state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip)
-
+ppo_agent = PPO(state_dim, action_dim, hidden_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip)
 
 num_epochs = 50000
 update_epoch = 8
 maxR = -200
 os.makedirs('./data/', exist_ok=True)
-for i_episode in range(num_epochs):    
-    
+for i_episode in range(num_epochs):
+
     state = env.reset()
     current_ep_reward = 0
 
     for t in range(300):
-        
+
         # select action with policy
         action = ppo_agent.select_action(state)
         state, reward, done, _ = env.step(action)
-        
+
         # saving reward and is_terminals
         ppo_agent.buffer.rewards.append(reward)
         ppo_agent.buffer.is_terminals.append(done)
-        
-        current_ep_reward += reward
 
-        # update PPO agent
-        #if time_step % update_timestep == 0:
-        #    ppo_agent.update()
+        current_ep_reward += reward
 
         if done:
             if i_episode % update_epoch == 0:
